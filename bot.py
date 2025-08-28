2c47c08d248163c27e006f9d2ce13bed0f264a37
decrescent <decrescent@hackclub.app>
2025-08-28 08:15:39 +0000

    forgot intend

--- bot.py
+++ bot.py
@@ -221,13 +221,13 @@ async def handle_view_task(ctx):
             
             if not row[2]:
 
-            no_tasks_now_embed = discord.Embed(
-                        title="No tasks right now. You'll be notified when tasks are assigned next!",
-                        color=ctx.author.accent_color
-                    )
-                    submitted_embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar)
+                no_tasks_now_embed = discord.Embed(
+                            title="No tasks right now. You'll be notified when tasks are assigned next!",
+                            color=ctx.author.accent_color
+                        )
+                        submitted_embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar)
 
-                    await ctx.respond(embed=submitted_embed)
+                        await ctx.respond(embed=submitted_embed)
 		
             else:
 
